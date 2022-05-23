cefd51319982bc4f29cc7f73e7046a5fdcc7f3d3
Irfan Rasheed <102538017+Irfan-Rasheed@users.noreply.github.com>
2022-05-23 23:26:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Task 07
+
+
+        //Task 01
+        window.alert("Welcome to Our Caterig Service!")
+
+        //Task 02
+        window.alert("Please Enter the Valid and Correct Information")
+
+        //Task 03
+        window.alert("Once Again Most Welcome to our Website! \n I hope you will enjoy our Menu")
+
+        //Task 04
+        window.alert("Welcome to our Catering Services")
+
+        window.alert("Happy Coding \n Prevent this page from creating additional dialogs.")
+
+        //Task 05
+        console.log("Hello.... I can run JS through my web browser's console.")
+
+
